b7cf2b43a7a93597c190193a9dc064ce9265b8d4
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2024-11-27 10:42:57 +0530

    homesection1, 2 images added

--- src/components/HomeSection1.js
+++ src/components/HomeSection1.js
@@ -1,10 +1,13 @@
 //import Button from "./Button";
 import CardType1 from "./CardType1";
+import card_1 from "../images/landing/Untitled-design-1.png";
+import card_2 from "../images/landing/Untitled-design-2.png";
+import card_3 from "../images/landing/Untitled-design-3.png";
 
 function HomeSection1() {
   return (
     <div className="h-[100dvh] flex flex-col items-start py-12 px-3 gap-6 max-[600px]:h-full">
-      <span className="g-font-2 text-xs tracking-widest text-gray-600 uppercase flex items-center gap-5">
+      <span className="flex items-center gap-5 text-xs tracking-widest text-gray-600 uppercase g-font-2">
         New
         <div className="w-[120px] border-t-[1px] border-t-gray-400"></div>
       </span>
@@ -16,9 +19,7 @@ function HomeSection1() {
             cardText1={
               "Experience Royal Havenwood communities and ammenities from the comfort of your home."
             }
-            cardImage={
-              "https://royalhavenwood.com/wp-content/uploads/2024/07/Untitled-design-1.png"
-            }
+            cardImage={card_1}
             extraClasses={"h-[70dvh] w-[400px] max-[600px]:w-full"}
             btnText={"Try It Now"}
           />
@@ -26,9 +27,7 @@ function HomeSection1() {
             cardHeader={"Intenational Projects"}
             cardHeaderClasses={"text-4xl"}
             cardText1={"Explore our portfolio of international projects."}
-            cardImage={
-              "https://royalhavenwood.com/wp-content/uploads/2024/07/Untitled-design-2.png"
-            }
+            cardImage={card_2}
             extraClasses={"h-[70dvh] w-[400px] max-[600px]:w-full"}
             btnText={"Read More"}
           />
@@ -38,9 +37,7 @@ function HomeSection1() {
             cardText1={
               "Click to read our top tips and tricks for property management, home decoration and more."
             }
-            cardImage={
-              "https://royalhavenwood.com/wp-content/uploads/2024/07/Untitled-design-3.png"
-            }
+            cardImage={card_3}
             extraClasses={"h-[70dvh] w-[400px] max-[600px]:w-full"}
             btnText={"Read More"}
           />
--- src/components/HomeSection2.js
+++ src/components/HomeSection2.js
@@ -1,19 +1,24 @@
 import Button from "./Button";
 import CardType2 from "./CardType2";
+import img_1 from "../images/landing/Hero-1-scaled.jpg";
+import img_2 from "../images/landing/About-us-scaled.jpg";
+import img_3 from "../images/landing/Interior-1-scaled.jpg";
+import img_4 from "../images/landing/laura-adai-J60bPeDiR8A-unsplash-scaled.jpg";
+import img_5 from "../images/landing/HERO_1620X832_2-2-706x385.jpg";
 
 function HomeSection2() {
   return (
     <div className="flex min-h-[100dvh] max-[992px]:flex-col lg:h-fit">
       {/*Title*/}
       <div className="flex flex-col items-start gap-5 w-1/3 px-3 py-12 max-[992px]:w-full lg:h-fit">
-        <span className="g-font-2 text-xs tracking-widest text-gray-600 uppercase flex items-center gap-5">
+        <span className="flex items-center gap-5 text-xs tracking-widest text-gray-600 uppercase g-font-2">
           Living spaces
           <div className="w-[120px] border-t-[1px] border-t-gray-400"></div>
         </span>
-        <h1 className="uppercase text-5xl max-lg:text-4xl leading-snug">
+        <h1 className="text-5xl leading-snug uppercase max-lg:text-4xl">
           Architecture And Decor
         </h1>
-        <p className="text-lg g-font-1 text-gray-600">
+        <p className="text-lg text-gray-600 g-font-1">
           At Royal Havenwood, we blend modern and classic architecture to create
           stunning, durable properties. Our interiors are designed with stylish
           furniture, soothing colors, and elegant decor, ensuring every space is
@@ -38,20 +43,14 @@ function HomeSection2() {
             cardImage={
               "https://royalhavenwood.com/wp-content/uploads/2024/11/Second-scaled.jpg"
             }
-            cardImageMd={
-              "https://cdn.properties.emaar.com/wp-content/uploads/2024/10/HERO_1620X832_2-2-706x385.jpg"
-            }
+            cardImageMd={img_5}
           />
           <CardType2
             cardUrl={"https://www.google.com"}
             cardText1={"Polished, tropical hotel with tennis"}
             cardText2={"Hotel Sigiriya"}
-            cardImage={
-              "https://royalhavenwood.com/wp-content/uploads/2024/11/Hero-1-scaled.jpg"
-            }
-            cardImageMd={
-              "https://royalhavenwood.com/wp-content/uploads/2024/11/Hero-1-scaled.jpg"
-            }
+            cardImage={img_1}
+            cardImageMd={img_1}
           />
         </div>
         {/*Card Stock 2*/}
@@ -60,34 +59,22 @@ function HomeSection2() {
             cardUrl={"https://www.google.com"}
             cardText1={"Center Point Grill & Bar"}
             cardText2={"Jetwing Blue Negombo"}
-            cardImage={
-              "https://royalhavenwood.com/wp-content/uploads/2024/11/About-us-scaled.jpg"
-            }
-            cardImageMd={
-              "https://royalhavenwood.com/wp-content/uploads/2024/11/About-us-scaled.jpg"
-            }
+            cardImage={img_2}
+            cardImageMd={img_2}
           />
           <CardType2
             cardUrl={"https://www.google.com"}
             cardText1={"Baywatch Sea Food"}
             cardText2={"Dickwella Resort and Spa"}
-            cardImage={
-              "https://royalhavenwood.com/wp-content/uploads/2024/11/Interior-1-scaled.jpg"
-            }
-            cardImageMd={
-              "https://royalhavenwood.com/wp-content/uploads/2024/11/Interior-1-scaled.jpg"
-            }
+            cardImage={img_3}
+            cardImageMd={img_3}
           />
           <CardType2
             cardUrl={"https://www.google.com"}
             cardText1={"Overlooking Weligama Bay"}
             cardText2={"Weligama Bay Resort"}
-            cardImage={
-              "https://royalhavenwood.com/wp-content/uploads/2024/11/laura-adai-J60bPeDiR8A-unsplash-scaled.jpg"
-            }
-            cardImageMd={
-              "https://royalhavenwood.com/wp-content/uploads/2024/11/laura-adai-J60bPeDiR8A-unsplash-scaled.jpg"
-            }
+            cardImage={img_4}
+            cardImageMd={img_4}
           />
         </div>
       </div>
