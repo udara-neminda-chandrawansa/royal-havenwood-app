b3be328c024ef7030dedbc9743c170d64a70c432
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2024-11-28 10:22:01 +0530

    about page made responsive

--- src/About.js
+++ src/About.js
@@ -63,7 +63,7 @@ export default function About() {
   return (
     <>
       {/*banner + nav*/}
-      <div className="text-black flex h-[90dvh] w-full">
+      <div className="flex w-full text-black h-fit">
         <Sidebar></Sidebar>
         <div className="flex flex-col h-fit">
           {/*nav*/}
@@ -71,19 +71,19 @@ export default function About() {
             <Nav theme={"dark"}></Nav>
           </span>
           {/*banner (img + overview text)*/}
-          <div>
+          <div className="relative">
             <img
               src="https://cdn.properties.emaar.com/wp-content/uploads/2020/07/BK_2600x1470-scaled-1-1620x740.jpg"
               alt="about"
             ></img>
             {/*floating white square with text*/}
-            <div className="absolute top-[450px] w-full h-fit max-[992px]:relative max-[992px]:top-5">
+            <div className="absolute bottom-[-15%] w-full h-fit max-[992px]:relative max-[992px]:top-0">
               <div className="bg-white w-2/3 max-[992px]:w-full p-4 py-12 h-fit flex flex-col justify-center">
                 <span className="flex items-center gap-5 p-1 text-xs tracking-widest text-gray-600 uppercase g-font-2">
                   Overview
                   <div className="w-[120px] border-t-[1px] border-t-gray-400"></div>
                 </span>
-                <h1 className="text-5xl uppercase max-sm:text-3xl">
+                <h1 className="text-5xl uppercase max-xl:text-4xl max-lg:text-3xl max-md:text-2xl max-sm:text-xl">
                   About Royal Havenwood Properties
                 </h1>
                 <p className="py-4 text-lg g-font-1 max-sm:text-sm">
@@ -105,13 +105,13 @@ export default function About() {
         </div>
       </div>
       {/*owners message*/}
-      <div className="p-4 mt-[150px] h-[70dvh] max-[992px]:h-[90dvh] max-sm:h-[80dvh] max-[992px]:flex flex-col justify-center items-center max-[992px]:mt-0">
-        <img
+      <div className="relative p-4 mt-[10%] max-[992px]:flex flex-col justify-center items-center max-[992px]:mt-0">
+        <img className="w-full"
           src="https://cdn.properties.emaar.com/wp-content/uploads/2022/04/chairman-image-1620x740-1-1620x740.png"
           alt="founder"
         ></img>
         {/*floating white square with text*/}
-        <div className="absolute top-[140dvh] h-fit max-[992px]:relative max-[992px]:top-5">
+        <div className="absolute bottom-0 h-fit max-[992px]:static">
           <div className="bg-white w-2/5 max-[992px]:w-full p-8 py-12 h-fit flex flex-col justify-center gap-3">
             <h1 className="text-2xl font-medium uppercase">
               "WE ARE SHARPENING OUR STRATEGY TO BE ONE OF THE WORLD'S MOST
@@ -130,17 +130,17 @@ export default function About() {
         </div>
       </div>
       {/*who we are*/}
-      <div className="h-[70dvh] flex flex-col justify-center gap-4 max-[992px]:h-fit max-[992px]:justify-start">
-        <h1 className="text-5xl text-center uppercase max-sm:text-3xl">
+      <div className="h-fit py-6 flex flex-col justify-center gap-4 max-[992px]:h-fit max-[992px]:justify-start">
+        <h1 className="text-5xl text-center uppercase max-xl:text-4xl max-lg:text-3xl max-md:text-2xl max-sm:text-xl">
           who we are
         </h1>
         {/*card container*/}
         <div className="w-full h-fit flex items-center p-4 gap-3 max-[992px]:flex-col">
           <div className="bg-gray-50 flex flex-col justify-center gap-6 p-8 h-[400px] w-1/3 max-[992px]:w-full">
-            <h1 className="text-2xl uppercase max-sm:text-xl">
+            <h1 className="text-2xl uppercase max-xl:text-xl max-lg:text-lg">
               Royal Havenwood PROPERTIES BOARD OF DIRECTORS
             </h1>
-            <p className="text-xl g-font-1 max-sm:text-lg">
+            <p className="text-xl g-font-1 max-xl:text-lg">
               Royal Havenwood's leadership team is comprised of the industry's most
               experienced leaders, who have secured our place as one of the
               world's greatest real estate and lifestyle developers.
@@ -153,10 +153,10 @@ export default function About() {
             ></Button>
           </div>
           <div className="bg-gray-50 flex flex-col justify-center gap-6 p-8 h-[400px] w-1/3 max-[992px]:w-full">
-            <h1 className="text-2xl uppercase max-sm:text-xl">
+            <h1 className="text-2xl uppercase max-xl:text-xl max-lg:text-lg">
               Royal Havenwood DEVELOPMENT BOARD OF DIRECTORS
             </h1>
-            <p className="text-xl g-font-1 max-sm:text-lg">
+            <p className="text-xl g-font-1 max-xl:text-lg">
               With their combined wisdom and innovative approach, Royal Havenwood
               Development's leadership has been essential in establishing Royal Havenwood as
               a top-tier real estate and lifestyle developer.
@@ -169,10 +169,10 @@ export default function About() {
             ></Button>
           </div>
           <div className="bg-gray-50 flex flex-col justify-center gap-6 p-8 h-[400px] w-1/3 max-[992px]:w-full">
-            <h1 className="text-2xl uppercase max-sm:text-xl">
+            <h1 className="text-2xl uppercase max-xl:text-xl max-lg:text-lg">
               PRINCIPAL OFFICERS
             </h1>
-            <p className="text-xl g-font-1 max-sm:text-lg">
+            <p className="text-xl g-font-1 max-xl:text-lg">
               Royal Havenwood has built a team of motivated officers, united in their
               commitment to excellence. This dedication stands strong, even
               amidst our many achievements and accolades, ensuring a constant
@@ -188,14 +188,14 @@ export default function About() {
         </div>
       </div>
       {/*int. destinations*/}
-      <div className="flex min-h-[100dvh] max-[992px]:flex-col lg:h-fit">
+      <div className="flex max-[992px]:flex-col lg:h-fit">
         {/*Title*/}
         <div className="flex flex-col items-start gap-5 w-1/3 px-3 py-12 max-[992px]:w-full lg:h-fit">
           <span className="flex items-center gap-5 text-xs tracking-widest text-gray-600 uppercase g-font-2">
             Properties
             <div className="w-[120px] border-t-[1px] border-t-gray-400"></div>
           </span>
-          <h1 className="text-5xl leading-snug uppercase max-lg:text-4xl">
+          <h1 className="text-5xl leading-snug uppercase max-xl:text-4xl max-lg:text-3xl max-md:text-2xl max-sm:text-xl">
             International destinations
           </h1>
           <p className="text-lg text-gray-600 g-font-1">
@@ -285,14 +285,14 @@ export default function About() {
         </span>
       </div>
       {/*other Royal Havenwood businesses*/}
-      <div className="h-[100dvh] flex items-center justify-center px-3 py-12 max-lg:h-fit">
+      <div className="flex items-center justify-center px-3 py-12 h-fit max-lg:h-fit">
         <div className="flex flex-col w-full gap-3">
           <span className="flex items-center gap-5 text-xs tracking-widest text-gray-600 uppercase g-font-2">
             More
             <div className="w-[120px] border-t-[1px] border-t-gray-400"></div>
           </span>
           <span className="flex items-center justify-between">
-            <h1 className="text-5xl leading-snug uppercase max-lg:text-4xl">
+            <h1 className="text-5xl leading-snug uppercase max-xl:text-4xl max-lg:text-3xl max-md:text-2xl max-sm:text-xl">
               OTHER Royal Havenwood BUSINESSES
             </h1>
           </span>
@@ -300,7 +300,7 @@ export default function About() {
             <span className="flex justify-between w-full gap-3 py-3 max-md:flex-col">
               <CardType1
                 cardHeader={"Royal Havenwood HOSPITALITY"}
-                cardHeaderClasses={"text-2xl"}
+                cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
                 cardImage={
                   "https://cdn.properties.emaar.com/wp-content/uploads/2020/06/emaar-hospitality-320x415.jpg"
                 }
@@ -310,7 +310,7 @@ export default function About() {
               />
               <CardType1
                 cardHeader={"Royal Havenwood MALLS"}
-                cardHeaderClasses={"text-2xl"}
+                cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
                 cardImage={
                   "https://cdn.properties.emaar.com/wp-content/uploads/2020/06/emaar-malls-320x415.jpg"
                 }
@@ -320,7 +320,7 @@ export default function About() {
               />
               <CardType1
                 cardHeader={"BURJ KHALIFA"}
-                cardHeaderClasses={"text-2xl"}
+                cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
                 cardImage={
                   "https://cdn.properties.emaar.com/wp-content/uploads/2020/06/burj-khalifa-320x415.jpg"
                 }
@@ -330,7 +330,7 @@ export default function About() {
               />
               <CardType1
                 cardHeader={"Royal Havenwood ENTERTAINMENT"}
-                cardHeaderClasses={"text-2xl"}
+                cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
                 cardImage={
                   "https://cdn.properties.emaar.com/wp-content/uploads/2020/06/emaar-entertainment-320x415.jpg"
                 }
@@ -343,7 +343,7 @@ export default function About() {
         </div>
       </div>
       {/*3 cards - card container*/}
-      <div className="bg-gray-50 h-[80dvh] flex justify-between items-center gap-8 px-4 max-[992px]:h-fit max-[992px]:flex-col">
+      <div className="bg-gray-50 h-fit py-6 flex justify-between items-center gap-8 px-4 max-[992px]:h-fit max-[992px]:flex-col">
         <CardType3
           titleText={"INVESTOR RELATIONS"}
           contentText={
@@ -376,9 +376,9 @@ export default function About() {
         />
       </div>
       {/*featured awards*/}
-      <div className="h-[100dvh] bg-white flex flex-col justify-center w-full">
+      <div className="flex flex-col justify-center w-full py-6 bg-white h-fit">
         {/*heading*/}
-        <h1 className="text-5xl leading-snug text-center uppercase max-lg:text-4xl">
+        <h1 className="text-5xl leading-snug text-center uppercase max-xl:text-4xl max-lg:text-3xl max-md:text-2xl max-sm:text-xl">
           Featured awards
         </h1>
         {/*cards*/}
@@ -390,7 +390,7 @@ export default function About() {
           <span className="flex gap-5 py-3 w-fit">
             <CardType1
               cardHeader={"Certified Autism Center™"}
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"Designation from IBCCES"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -406,7 +406,7 @@ export default function About() {
             />
             <CardType1
               cardHeader={"LEED Platinum Certification"}
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"Operations and Maintenance: Existing Buildings"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -422,7 +422,7 @@ export default function About() {
             />
             <CardType1
               cardHeader={"Developer of the Year"}
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"Arabian Business Achievement Awards"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -438,7 +438,7 @@ export default function About() {
             />
             <CardType1
               cardHeader={"KidZania- Best Edutainment Center"}
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"MENALAC"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -454,7 +454,7 @@ export default function About() {
             />
             <CardType1
               cardHeader={"Reel Cinemas - Best Cinema Experience"}
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"MENALAC"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -472,7 +472,7 @@ export default function About() {
               cardHeader={
                 "Skyviews Observatory - Most Unique Visitor Attraction"
               }
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"MENALAC"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -490,7 +490,7 @@ export default function About() {
               cardHeader={
                 "Address Grand Creek Harbour - Highly Commended “FM Mobilisation of the Year“"
               }
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"Facilities Management Awards, Middle East"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -506,7 +506,7 @@ export default function About() {
             />
             <CardType1
               cardHeader={"Dubai Hills Park -  Sustainability Prize"}
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={
                 "Landscape Middle East Awards in Parks &amp; Recreation"
               }
@@ -524,7 +524,7 @@ export default function About() {
             />
             <CardType1
               cardHeader={"ECM - Overall Winner Award"}
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"Gulf Real Estate Awards"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -540,7 +540,7 @@ export default function About() {
             />
             <CardType1
               cardHeader={"ECM - Community Management Company of the Year"}
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"Smart Built Environment Awards (SBE)"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -556,7 +556,7 @@ export default function About() {
             />
             <CardType1
               cardHeader={"ECM - Happiest Residential Community Of The Year"}
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"IRECMS Dubai Awards"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -572,7 +572,7 @@ export default function About() {
             />
             <CardType1
               cardHeader={"ECM - Community Engagement Initiative of the Year"}
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"Smart Built Environment Awards (SBE)"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -590,7 +590,7 @@ export default function About() {
               cardHeader={
                 "The Storm Coaster -  World’s Fastest Vertical-Launch Rollercoaster"
               }
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"Guinness World Records™"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -608,7 +608,7 @@ export default function About() {
               cardHeader={
                 "Address Beach Resort - World’s tallest infinity pool"
               }
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"Guinness World Records™"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -624,7 +624,7 @@ export default function About() {
             />
             <CardType1
               cardHeader={"Dubai Mall - Best Customer Experience"}
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"World Retail Awards"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -640,7 +640,7 @@ export default function About() {
             />
             <CardType1
               cardHeader={"Address Downtown - World Leading Luxury City Hotel"}
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"World Travel Awards™"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -658,7 +658,7 @@ export default function About() {
               cardHeader={
                 "Dubai Aquarium & Underwater Zoo - Largest OLED screen"
               }
-              cardHeaderClasses={"text-2xl"}
+              cardHeaderClasses={"text-2xl max-xl:text-xl max-lg:text-lg"}
               cardText1={"Guinness World Records™"}
               cardText1Classes={
                 "uppercase text-xs g-font-2 tracking-widest p-1"
@@ -674,8 +674,8 @@ export default function About() {
             />
           </span>
         </div>
-        {/*buttons*/}
-        <span className="flex items-center justify-center">
+        {/*Left, carousel buttons, Right*/}
+        <span className="flex items-center justify-center max-md:hidden">
           <Button
             displayText={
               <svg
@@ -698,7 +698,7 @@ export default function About() {
             extraClasses={"w-[90px]"}
           />
           {/*carousel buttons*/}
-          <div className="h-[50px] flex items-center justify-center mb-2">
+          <div className="h-[50px] flex items-center justify-center mb-2 max-xl:hidden">
             <div className="flex items-center gap-3">
               <div
                 className="z-10 w-3 h-3 bg-black rounded car-btn"
@@ -779,6 +779,7 @@ export default function About() {
             extraClasses={"w-[90px]"}
           />
         </span>
+        {/*view all awards btn*/}
         <span className="flex justify-center">
           <Button
             displayText={"View All Awards"}
--- src/components/CardType3.js
+++ src/components/CardType3.js
@@ -6,16 +6,16 @@ function CardType3({titleText, contentText, imgSrc, extraClasses}) {
         alt="img"
         className="h-[300px] object-cover"
       ></img>
-      <div className="p-8 flex flex-col gap-3">
-        <h1 className="text-2xl uppercase max-sm:text-xl">
+      <div className="flex flex-col gap-3 p-8">
+        <h1 className="text-2xl uppercase max-xl:text-xl max-lg:text-lg">
           {titleText}
         </h1>
-        <p className="text-xl g-font-1 max-sm:text-lg">
+        <p className="text-xl max-xl:text-lg max-lg:text-base max-md:text-sm g-font-1">
           {contentText}
         </p>
         <a
           href="https://www.google.com"
-          className="hover:text-sky-500 uppercase g-font-2 text-xs tracking-widest w-fit"
+          className="text-xs tracking-widest uppercase hover:text-sky-500 g-font-2 w-fit"
         >
           Learn More
         </a>
